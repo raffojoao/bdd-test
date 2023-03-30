5277bf1e6d4a9ccec60e4cb2646c9bbf4cb7885d
João Raffo <jomf@gft.com>
2023-03-30 17:48:41 -0300

    reset sh and yml

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Get the token from Travis environment vars and build the bot URL:
 BOT_URL="https://api.telegram.org/bot5616652514:AAGJrfbzOTiai5v8kkHinfPTfnrwiW0fDw4/sendMessage"
@@ -26,34 +26,21 @@ fi
 
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
-# send_msg () {
-    # curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
-    #     -d text="${MESSAGE}" -d parse_mode=${PARSE_MODE}
-# }
-
-echo "Sending info message..."
-  # send telegram chat message
-  curl \
-    --silent \
-    -X POST "${BOT_URL}" \
-    -d "chat_id=${TELEGRAM_CHAT_ID}" \
-    -d "text=${MESSAGE}" \
-    # -d "reply_to_message_id=${MESSAGE_ID}" \
-    -d "parse_mode=Markdown" \
-    -d "disable_web_page_preview=true" \
-    >/dev/null
-
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=${PARSE_MODE}
+}
 
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
-# send_msg "
-# -------------------------------------
-# Travis build *${build_status}!*
-# \`Repository:  ${TRAVIS_REPO_SLUG}\`
-# \`Branch:      ${TRAVIS_BRANCH}\`
-# *Commit Msg:*
-# ${TRAVIS_COMMIT_MESSAGE}
-# [Job Log here](${TRAVIS_JOB_WEB_URL})
-# --------------------------------------
-# "
+send_msg "
+-------------------------------------
+Travis build *${build_status}!*
+\`Repository:  ${TRAVIS_REPO_SLUG}\`
+\`Branch:      ${TRAVIS_BRANCH}\`
+*Commit Msg:*
+${TRAVIS_COMMIT_MESSAGE}
+[Job Log here](${TRAVIS_JOB_WEB_URL})
+--------------------------------------
+"
